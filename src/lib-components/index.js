599115b42b13787de624efa62837fbd3ce1c433e
Philippe Beck <philippe@philippebeck.net>
2022-05-13 22:42:42 +0200

    Create components index

---
+++ src/lib-components/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+export { default as FieldElt } from './FieldElt';
+export { default as FootElt } from './FootElt';
+export { default as ListElt } from './ListElt';
+export { default as TableElt } from './TableElt';
