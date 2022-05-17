84f976714cc5ea25d393525530c9aee982fd28de
Philippe Beck <philippe@philippebeck.net>
2022-05-17 08:07:12 +0200

    Create CSS vars for hsl & hsla

--- src/lib-components/FootElt.vue
+++ src/lib-components/FootElt.vue
@@ -52,6 +52,18 @@ export default {
 </script>
 
 <style>
+/* INIT */
+
+footer {
+  --foot-main-bg-color: hsla(240,67%,94%, 0.9); /* Lavender */
+  --foot-main-link-color: hsl(249,80%,67%); /* MediumSlateBlue */
+  --foot-second-bg-color: hsla(208,100%,97%, 0.9); /* AliceBlue */
+  --foot-second-link-color: hsl(210,100%,56%); /* DodgerBlue */
+  --foot-title-color: hsl(0,0%,41%); /* DimGray */
+
+  text-align: center;
+}
+
 ul,
 h3 {
   margin: 0;
@@ -68,25 +80,24 @@ a {
 
 /* MAIN PART */
 
-footer > ul {
+footer > ul { 
   display: flex;
   flex-direction: column;
   place-items: center;
   gap: 1rem;
   padding: 1rem;
-  text-align: center;
-  background-color: hsl(240,67%,94%); /* Lavender */
+  background-color: var(--foot-main-bg-color);
 }
 
 h3 {
   margin-bottom: 0.4rem;
   font-size: 1.3rem;
   font-family: monospace;
-  color: hsl(0,0%,41%); /* DimGray */
+  color: var(--foot-title-color);
 }
 
 section a {
-  color: hsl(249,80%,67%); /* MediumSlateBlue */
+  color: var(--foot-main-link-color);
 }
 
 /* SECOND PART */
@@ -97,11 +108,11 @@ aside > ul {
   place-content: center;
   gap: 1rem;
   padding: 1rem;
-  background-color: hsl(208,100%,97%); /* AliceBlue */
+  background-color: var(--foot-second-bg-color);
 }
 
 aside a {
-  color: hsl(210,100%,56%); /* DodgerBlue */
+  color: var(--foot-second-link-color);
 }
 
 /* RESPONSIVE */
