ff95f2a7ec985b652ed12811760806d7c5df5b13
Philippe Beck <philippe@philippebeck.net>
2022-05-26 19:11:31 +0200

    Refactoring style & add color classes to BtnElt

--- src/lib-components/BtnElt.vue
+++ src/lib-components/BtnElt.vue
@@ -104,60 +104,206 @@
 }
 </style>
 
+<style scoped>
 a,
 button {
   display: inline-block;
   margin: 10px;
-  padding: 10px;
-  font-size: 1.5rem;
+  border: solid medium hsl(var(--black));
+  border-radius: 10px;
+  padding: 5px 10px;
+  font-family: var(--monospace);
+  font-size: 1.2rem;
   font-weight: bold;
   text-align: center;
+  text-decoration: none;
+  background-color: hsl(var(--black));
+  color: hsl(var(--white));
   cursor: pointer;
 }
 
-a {
-  border: solid medium var(--white);
-  border-radius: 10px;
-  background-color: var(--black);
-  color: var(--white);
-  text-decoration: none;
-}
-
-button {
-  border: outset medium var(--black);
-  border-radius: 5px;
-  background-color: var(--white);
-  color: var(--black);
-}
-
 a:hover,
-button:hover,
 a:focus,
-button:focus {
-  border-radius: 5px;
-  transition: all 300ms linear;
-  animation: none;
-}
-
-a:hover,
-a:focus {
-  border: solid medium var(--black);
-  box-shadow: inset 0 0 5px 5px;
-  background-color: var(--white);
-  color: var(--black);
-}
-
 button:hover,
 button:focus {
-  border: solid medium var(--white);
-  box-shadow: inset 0 0 5px 5px;
-  background-color: var(--black);
-  color: var(--white);
+  border-radius: 5px;
+  background-color: hsl(var(--white));
+  color: hsl(var(--black));
+  box-shadow: inset 0 0 5px 2px;
+  transition: all 500ms linear;
 }
 
 a > *,
 button > * {
   display: block;
   margin: auto;
+}
+
+.blue {
+  border-color: hsl(var(--blue));
+  background-color: hsl(var(--blue));
+  color: hsl(var(--white));
+}
+
+.blue:hover,
+.blue:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--blue));
+}
+
+.cyan {
+  border-color: hsl(var(--cyan));
+  background-color: hsl(var(--cyan));
+  color: hsl(var(--white));
+}
+
+.cyan:hover,
+.cyan:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--cyan));
+}
+
+.gray {
+  border-color: hsl(var(--gray));
+  background-color: hsl(var(--gray));
+  color: hsl(var(--white));
+}
+
+.gray:hover,
+.gray:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--gray));
+}
+
+.green {
+  border-color: hsl(var(--green));
+  background-color: hsl(var(--green));
+  color: hsl(var(--white));
+}
+
+.green:hover,
+.green:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--green));
+}
+
+.lime {
+  border-color: hsl(var(--lime));
+  background-color: hsl(var(--lime));
+  color: hsl(var(--white));
+}
+
+.lime:hover,
+.lime:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--lime));
+}
+
+.maroon {
+  border-color: hsl(var(--maroon));
+  background-color: hsl(var(--maroon));
+  color: hsl(var(--white));
+}
+
+.maroon:hover,
+.maroon:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--maroon));
+}
+
+.navy {
+  border-color: hsl(var(--navy));
+  background-color: hsl(var(--navy));
+  color: hsl(var(--white));
+}
+
+.navy:hover,
+.navy:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--navy));
+}
+
+.orange {
+  border-color: hsl(var(--orange));
+  background-color: hsl(var(--orange));
+  color: hsl(var(--white));
+}
+
+.orange:hover,
+.orange:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--orange));
+}
+
+.purple {
+  border-color: hsl(var(--purple));
+  background-color: hsl(var(--purple));
+  color: hsl(var(--white));
+}
+
+.purple:hover,
+.purple:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--purple));
+}
+
+.red {
+  border-color: hsl(var(--red));
+  background-color: hsl(var(--red));
+  color: hsl(var(--white));
+}
+
+.red:hover,
+.red:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--red));
+}
+
+.silver {
+  border-color: hsl(var(--silver));
+  background-color: hsl(var(--silver));
+  color: hsl(var(--gray));
+}
+
+.silver:hover,
+.silver:focus {
+  background-color: hsl(var(--gray));
+  color: hsl(var(--silver));
+}
+
+.teal {
+  border-color: hsl(var(--teal));
+  background-color: hsl(var(--teal));
+  color: hsl(var(--white));
+}
+
+.teal:hover,
+.teal:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--teal));
+}
+
+.violet {
+  border-color: hsl(var(--violet));
+  background-color: hsl(var(--violet));
+  color: hsl(var(--white));
+}
+
+.violet:hover,
+.violet:focus {
+  background-color: hsl(var(--white));
+  color: hsl(var(--violet));
+}
+
+.yellow {
+  border-color: hsl(var(--yellow));
+  background-color: hsl(var(--yellow));
+  color: hsl(var(--gray));
+}
+
+.yellow:hover,
+.yellow:focus {
+  background-color: hsl(var(--gray));
+  color: hsl(var(--yellow));
 }
 </style>
