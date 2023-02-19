87f5651f65100b29dbcfc3db45ed5f28049b5f16
Philippe Beck <philippe@philippebeck.net>
2023-02-19 11:14:58 +0400

    Update lib index for components path

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable import/prefer-default-export */
-export { default as BtnElt } from './base/BtnElt';
-export { default as FieldElt } from './base/FieldElt';
-export { default as MediaElt } from './base/MediaElt';
+export { default as BtnElt } from './base/btn/BtnElt';
+export { default as FieldElt } from './base/field/FieldElt';
+export { default as MediaElt } from './base/media/MediaElt';
 
-export { default as ListElt } from './data/ListElt';
-export { default as SliderElt } from './data/SliderElt';
-export { default as TableElt } from './data/TableElt';
+export { default as ListElt } from './data/list/ListElt';
+export { default as SliderElt } from './data/slider/SliderElt';
+export { default as TableElt } from './data/table/TableElt';
 
-export { default as CardElt } from './main/CardElt';
-export { default as FootElt } from './main/FootElt';
-export { default as NavElt } from './main/NavElt';
+export { default as CardElt } from './main/card/CardElt';
+export { default as FootElt } from './main/foot/FootElt';
+export { default as NavElt } from './main/nav/NavElt';
 
