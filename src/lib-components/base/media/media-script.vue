1186bb2fa9ead7206806f66bbed802f3758cfceb
Philippe Beck <philippe@philippebeck.net>
2023-02-20 09:25:43 +0400

    Create media separate files to get css vars

---
+++ src/lib-components/base/media/media-script.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  name: "MediaElt",
+
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String
+    },
+    title: {
+      type: String
+    },
+    type: {
+      type: String,
+      default: ""
+    },
+    medias: {
+      type: Array
+    },
+    loop: {
+      type: Boolean,
+      default: false
+    },
+    height: {
+      type: Number
+    },
+    width: {
+      type: Number,
+      default: 300
+    }
+  },
+  
+  methods: {
+    hasSlot(name) {
+      return this.$slots[name] !== undefined;
+    }
+  }
+}
+</script>
+
+<style>
