55c0637dbd0e6111a402c97820d80f6f61612513
Philippe Beck <philippe@philippebeck.net>
2023-02-20 09:25:52 +0400

    Create list separate files to get css vars

---
+++ src/lib-components/data/ListElt.vue
@@ -0,0 +1,109 @@
+<template>
+  <!-- LIST ELT (Dynamic Slot) -->
+  <ul v-if="dynamic === true">
+    <li v-for="(item, index) in items"
+      :key="index">
+
+      <slot name="items"
+        :index="index"
+        :item="item">
+        {{ item }}
+      </slot>
+      
+      <!-- Nested List (option) -->
+      <ul v-if="hasSlot('nested')">
+
+        <li v-for="(value, key) in item"
+          :key="key">
+
+          <slot name="nested"
+            :index="index"
+            :item="item"
+            :key="key"
+            :value="value">
+            {{ value }}
+          </slot>
+
+        </li>
+      </ul>
+    </li>
+  </ul>
+
+  <!-- LIST ELT (Static Slot) -->
+  <ul v-else>
+    <li v-for="(item, index) in items"
+      :key="index">
+
+      <slot :name="`item-${index + 1}`"
+        :index="index"
+        :item="item">
+        {{ item }}
+      </slot>
+      
+      <!-- Nested List (option) -->
+      <ul v-if="hasSlot('nested')">
+
+        <li v-for="(value, key) in item"
+          :key="key">
+
+          <slot :name="`nested-${key + 1}`"
+            :index="index"
+            :item="item"
+            :key="key"
+            :value="value">
+            {{ value }}
+          </slot>
+
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "ListElt",
+  props: {
+    items: {
+      type: Object,
+      required: true
+    },
+    dynamic: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    hasSlot(name) {
+      return this.$slots[name] !== undefined;
+    }
+  }
+}
+</script>
+
+<style>
+ul {
+  --ve-ul-margin: 20px 0;
+  --ve-ul-padding: 0;
+  --ve-ul-list-style: none;
+  --ve-ul-text-align: center;
+  --ve-li-ul-display: flex;
+  --ve-li-ul-flex-flow: wrap;
+  --ve-li-ul-place-content: center;
+}
+</style>
+
+<style scoped>
+ul {
+  margin: var(--ve-ul-margin);
+  padding: var(--ve-ul-padding);
+  list-style: var(--ve-ul-list-style);
+  text-align: var(--ve-ul-text-align);
+}
+
+li > ul {
+  display: var(--ve-li-ul-display);
+  flex-flow: var(--ve-li-ul-flex-flow);
+  place-content: var(--ve-li-ul-place-content);
+}
+</style>
---
+++ src/lib-components/data/list/list-scoped-style.vue
@@ -0,0 +1,16 @@
+</style>
+
+<style scoped>
+ul {
+  margin: var(--ve-ul-margin);
+  padding: var(--ve-ul-padding);
+  list-style: var(--ve-ul-list-style);
+  text-align: var(--ve-ul-text-align);
+}
+
+li > ul {
+  display: var(--ve-li-ul-display);
+  flex-flow: var(--ve-li-ul-flex-flow);
+  place-content: var(--ve-li-ul-place-content);
+}
+</style>
