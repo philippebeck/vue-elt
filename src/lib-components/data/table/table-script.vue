08db7eda3a007968b7c02d2beab696029f9c6c86
Philippe Beck <philippe@philippebeck.net>
2023-02-20 09:26:12 +0400

    Create table separate files to get css vars

---
+++ src/lib-components/data/table/table-script.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "TableElt",
+  props: {
+    title: {
+      type: String
+    },
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    hasSlot(name) {
+      return this.$slots[name] !== undefined;
+    }
+  }
+}
+</script>
+
+<style>
