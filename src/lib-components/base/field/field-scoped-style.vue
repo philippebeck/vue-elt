b9c0ecfe69d88694749f63e45ece4c93b943731c
Philippe Beck <philippe@philippebeck.net>
2023-02-26 16:12:09 +0400

    Rename CSS vars to FieldElt

--- src/lib-components/base/field/field-scoped-style.vue
+++ src/lib-components/base/field/field-scoped-style.vue
@@ -2,50 +2,59 @@
 
 <style scoped>
 fieldset {
-  display: var(--ve-fieldset-display);
-  flex-flow: var(--ve-fieldset-flex-flow);
-  place-items: var(--ve-fieldset-place-items);;
-  margin: var(--ve-fieldset-margin);
-  border: var(--ve-fieldset-border);
-  padding: var(--ve-fieldset-padding);
-  max-width: var(--ve-fieldset-max-width);
-  text-align: var(--ve-fieldset-text-align);
-  transition: var(--ve-fieldset-transition);
+  display: var(--ve-field-display);
+  flex-flow: var(--ve-field-flex-flow);
+  place-items: var(--ve-field-place-items);;
+  margin: var(--ve-field-margin);
+  border: var(--ve-field-border);
+  padding: var(--ve-field-padding);
+  max-width: var(--ve-field-max-width);
+  text-align: var(--ve-field-text-align);
+  transition: var(--ve-field-transition);
 }
 
 fieldset > * {
-  box-sizing: var(--ve-field-box-sizing);
-  margin: var(--ve-field-margin);
-  padding: var(--ve-field-padding);
+  box-sizing: var(--ve-field-child-box-sizing);
+  margin: var(--ve-field-child-margin);
+  padding: var(--ve-field-child-padding);
 }
 
 legend {
-  width: var(--ve-legend-width);
-  font-size: var(--ve-legend-font-size);
-  color: var(--ve-legend-color);
+  width: var(--ve-field-legend-width);
+  font-size: var(--ve-field-legend-font-size);
+  color: var(--ve-field-legend-color);
+}
+
+fieldset:hover legend {
+  color: var(--ve-field-hover-legend-color);
 }
 
 label {
-  width: var(--ve-label-width);
-  visibility: var(--ve-label-visibility);
-  font-size: var(--ve-label-font-size);
-  font-style: var(--ve-label-font-style);
-  color: var(--ve-label-color);
+  width: var(--ve-field-label-width);
+  visibility: var(--ve-field-label-visibility);
+  font-size: var(--ve-field-label-font-size);
+  font-style: var(--ve-field-label-font-style);
+  color: var(--ve-field-label-color);
+}
+
+fieldset:hover > label,
+input:focus + label,
+select:focus + label,
+textarea:focus + label {
+  visibility: var(--ve-field-hover-label-visibility);
+  transform: var(--ve-field-hover-label-transform);
+  transition: var(--ve-field-hover-label-transition);
 }
 
 input,
 select,
 textarea {
-  border: var(--ve-input-border);
-  border-radius: var(--ve-input-border-radius);
-  outline: var(--ve-input-outline);
-  width: var(--ve-input-width);
-  line-height: var(--ve-input-line-height);
-  background-color: var(--ve-input-background-color);
-}
-
-fieldset:hover legend {
-  color: var(--ve-hover-legend-color);
+  border: var(--ve-field-input-border);
+  border-radius: var(--ve-field-input-border-radius);
+  outline: var(--ve-field-input-outline);
+  width: var(--ve-field-input-width);
+  line-height: var(--ve-field-input-line-height);
+  background-color: var(--ve-field-input-background-color);
 }
 
 fieldset:hover > input,
@@ -54,18 +63,9 @@ fieldset:hover > textarea,
 input:focus,
 select:focus,
 textarea:focus {
-  border: var(--ve-hover-input-border);
-  border-radius: var(--ve-hover-input-border-radius);
-  background-color: var(--ve-hover-input-background-color);
-  transition: var(--ve-hover-input-transition);
-}
-
-fieldset:hover > label,
-input:focus + label,
-select:focus + label,
-textarea:focus + label {
-  visibility: var(--ve-hover-label-visibility);
-  transform: var(--ve-hover-label-transform);
-  transition: var(--ve-hover-label-transition);
+  border: var(--ve-field-hover-input-border);
+  border-radius: var(--ve-field-hover-input-border-radius);
+  background-color: var(--ve-field-hover-input-background-color);
+  transition: var(--ve-field-hover-input-transition);
 }
 </style>
