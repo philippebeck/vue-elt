7a7ed89f933ffc97e39d6f9c03b343d5eafc0c0d
Philippe Beck <philippe@philippebeck.net>
2023-05-02 20:53:48 +0400

    Fix path to gruntfile for compile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,97 +4,97 @@ module.exports = function(grunt) {
     concat: {
       btn: {
         src: [
-          "src/lib-components/base/btn/btn-template.html",
-          "src/lib-components/base/btn/btn-script.vue",
-          "src/lib-components/base/btn/btn-var-style.css",
-          "src/lib-components/base/btn/btn-scoped-style.vue",
+          "src/lib-components/src/btn/btn-template.html",
+          "src/lib-components/src/btn/btn-script.vue",
+          "src/lib-components/src/btn/btn-var-style.css",
+          "src/lib-components/src/btn/btn-scoped-style.vue",
         ], 
-        dest: "src/lib-components/base/BtnElt.vue"
+        dest: "src/lib-components/BtnElt.vue"
       },
       field: {
         src: [
-          "src/lib-components/base/field/field-template.html",
-          "src/lib-components/base/field/field-script.vue",
-          "src/lib-components/base/field/field-var-style.css",
-          "src/lib-components/base/field/field-scoped-style.vue",
+          "src/lib-components/src/field/field-template.html",
+          "src/lib-components/src/field/field-script.vue",
+          "src/lib-components/src/field/field-var-style.css",
+          "src/lib-components/src/field/field-scoped-style.vue",
         ], 
-        dest: "src/lib-components/base/FieldElt.vue"
+        dest: "src/lib-components/FieldElt.vue"
       },
       media: {
         src: [
-          "src/lib-components/base/media/media-template.html",
-          "src/lib-components/base/media/media-script.vue",
-          "src/lib-components/base/media/media-var-style.css",
-          "src/lib-components/base/media/media-scoped-style.vue",
+          "src/lib-components/src/media/media-template.html",
+          "src/lib-components/src/media/media-script.vue",
+          "src/lib-components/src/media/media-var-style.css",
+          "src/lib-components/src/media/media-scoped-style.vue",
         ], 
-        dest: "src/lib-components/base/MediaElt.vue"
+        dest: "src/lib-components/MediaElt.vue"
       },
       list: {
         src: [
-          "src/lib-components/data/list/list-template.html",
-          "src/lib-components/data/list/list-script.vue",
-          "src/lib-components/data/list/list-var-style.css",
-          "src/lib-components/data/list/list-scoped-style.vue",
+          "src/lib-components/src/list/list-template.html",
+          "src/lib-components/src/list/list-script.vue",
+          "src/lib-components/src/list/list-var-style.css",
+          "src/lib-components/src/list/list-scoped-style.vue",
         ], 
-        dest: "src/lib-components/data/ListElt.vue"
+        dest: "src/lib-components/ListElt.vue"
       },
       slider: {
         src: [
-          "src/lib-components/data/slider/slider-template.html",
-          "src/lib-components/data/slider/slider-script.vue",
-          "src/lib-components/data/slider/slider-var-style.css",
-          "src/lib-components/data/slider/slider-scoped-style.vue",
+          "src/lib-components/src/slider/slider-template.html",
+          "src/lib-components/src/slider/slider-script.vue",
+          "src/lib-components/src/slider/slider-var-style.css",
+          "src/lib-components/src/slider/slider-scoped-style.vue",
         ], 
-        dest: "src/lib-components/data/SliderElt.vue"
+        dest: "src/lib-components/SliderElt.vue"
       },
       table: {
         src: [
-          "src/lib-components/data/table/table-template.html",
-          "src/lib-components/data/table/table-script.vue",
-          "src/lib-components/data/table/table-var-style.css",
-          "src/lib-components/data/table/table-scoped-style.vue",
+          "src/lib-components/src/table/table-template.html",
+          "src/lib-components/src/table/table-script.vue",
+          "src/lib-components/src/table/table-var-style.css",
+          "src/lib-components/src/table/table-scoped-style.vue",
         ], 
-        dest: "src/lib-components/data/TableElt.vue"
+        dest: "src/lib-components/TableElt.vue"
       },
       card: {
         src: [
-          "src/lib-components/main/card/card-template.html",
-          "src/lib-components/main/card/card-script.vue",
-          "src/lib-components/main/card/card-var-style.css",
-          "src/lib-components/main/card/card-scoped-style.vue",
+          "src/lib-components/src/card/card-template.html",
+          "src/lib-components/src/card/card-script.vue",
+          "src/lib-components/src/card/card-var-style.css",
+          "src/lib-components/src/card/card-scoped-style.vue",
         ], 
-        dest: "src/lib-components/main/CardElt.vue"
+        dest: "src/lib-components/CardElt.vue"
       },
       foot: {
         src: [
-          "src/lib-components/main/foot/foot-template.html",
-          "src/lib-components/main/foot/foot-script.vue",
-          "src/lib-components/main/foot/foot-var-style.css",
-          "src/lib-components/main/foot/foot-scoped-style.vue",
+          "src/lib-components/src/foot/foot-template.html",
+          "src/lib-components/src/foot/foot-script.vue",
+          "src/lib-components/src/foot/foot-var-style.css",
+          "src/lib-components/src/foot/foot-scoped-style.vue",
         ], 
-        dest: "src/lib-components/main/FootElt.vue"
+        dest: "src/lib-components/FootElt.vue"
       },
       nav: {
         src: [
-          "src/lib-components/main/nav/nav-template.html",
-          "src/lib-components/main/nav/nav-script.vue",
-          "src/lib-components/main/nav/nav-var-style.css",
-          "src/lib-components/main/nav/nav-scoped-style.vue",
+          "src/lib-components/src/nav/nav-template.html",
+          "src/lib-components/src/nav/nav-script.vue",
+          "src/lib-components/src/nav/nav-var-style.css",
+          "src/lib-components/src/nav/nav-scoped-style.vue",
         ], 
-        dest: "src/lib-components/main/NavElt.vue"
+        dest: "src/lib-components/NavElt.vue"
       },
       style: {
         src: [
           "node_modules/animadio/dist/style.css",
-          "src/lib-components/base/btn/btn-var-style.css",
-          "src/lib-components/base/field/field-var-style.css",
-          "src/lib-components/base/media/media-var-style.css",
-          "src/lib-components/data/list/list-var-style.css",
-          "src/lib-components/data/slider/slider-var-style.css",
-          "src/lib-components/data/table/table-var-style.css",
-          "src/lib-components/main/card/card-var-style.css",
-          "src/lib-components/main/foot/foot-var-style.css",
-          "src/lib-components/main/nav/nav-var-style.css",
+          "src/lib-components/src/btn/btn-var-style.css",
+          "src/lib-components/src/field/field-var-style.css",
+          "src/lib-components/src/media/media-var-style.css",
+          "src/lib-components/src/list/list-var-style.css",
+          "src/lib-components/src/slider/slider-var-style.css",
+          "src/lib-components/src/table/table-var-style.css",
+          "src/lib-components/src/card/card-var-style.css",
+          "src/lib-components/src/foot/foot-var-style.css",
+          "src/lib-components/src/nav/nav-var-style.css",
         ], 
         dest: "dist/style.css"
       },
