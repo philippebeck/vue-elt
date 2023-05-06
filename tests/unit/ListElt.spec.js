9d6dbe7e4a7aedecc1eb335c6ecfad40c2e13c75
Philippe Beck <philippe@philippebeck.net>
2023-05-06 12:41:34 +0400

    Finish to complete list tests

--- tests/unit/ListElt.spec.js
+++ tests/unit/ListElt.spec.js
@@ -26,6 +26,10 @@ describe("Default ListElt", () => {
     expect(wrapper.find("ul").exists()).toBe(true)
   })
 
+  test("must create a li element for each item", () => {
+    expect(wrapper.findAll("li").length).toBe(3)
+  })
+
   test("must have a props 'items' with 'Object' as type", () => {
     expect(typeof wrapper.props("items")).toBe("object")
   })
@@ -48,6 +52,51 @@ describe("Default ListElt", () => {
   })
 })
 
+/**
+ * ! DYNAMIC LIST
+ * @jest-environment jsdom
+ */
+describe("Dynamic ListElt", () => {
+  const wrapper = mount(ListElt, {
+    props: {
+      items: [
+        { id: 1, name: "Item 1" },
+        { id: 2, name: "Item 2" },
+        { id: 3, name: "Item 3" }
+      ],
+      dynamic: true
+    }
+  })
+
+  test("must create an ul element", () => {
+    expect(wrapper.find("ul").exists()).toBe(true)
+  })
+
+  test("must create a li element for each item", () => {
+    expect(wrapper.findAll("li").length).toBe(3)
+  })
+
+  test("must have a props 'items' with 'Object' as type", () => {
+    expect(typeof wrapper.props("items")).toBe("object")
+  })
+
+  test("must have a props 'items' with 'Item 1' as name of first item", () => {
+    expect(wrapper.props("items")[0].name).toBe("Item 1")
+  })
+
+  test("must have a props 'items' with 'Item 2' as name of second item", () => {
+    expect(wrapper.props("items")[1].name).toBe("Item 2")
+  })
+
+  test("must have a props 'items' with 'Item 3' as name of third item", () => {
+    expect(wrapper.props("items")[2].name).toBe("Item 3")
+  })
+
+  test("must have a props 'dynamic' with 'Boolean' as type & 'true' as value", () => {
+    expect(typeof wrapper.props("dynamic")).toBe("boolean")
+    expect(wrapper.props("dynamic")).toBe(true)
+  })
+})
 
 /**
  * ! NESTED LIST
@@ -67,8 +116,19 @@ describe("Nested ListElt", () => {
             ]
           }
         ]
+      },
+      slots: {
+        nested: "Test Nested"
       }
     })
+  })
+
+  test("must create an ul element", () => {
+    expect(wrapper.find("ul").exists()).toBe(true)
+  })
+
+  test("must create a li element for each item", () => {
+    expect(wrapper.findAll("li").length).toBe(4)
   })
 
   test("must have a props 'items' with 'Object' as type", () => {
@@ -86,36 +146,15 @@ describe("Nested ListElt", () => {
   test("must have a props 'items' with 'Item 3' as name of third item", () => {
     expect(wrapper.props("items")[0].children[1].name).toBe("Item 3")
   })
-})
 
-/**
- * DYNAMIC LIST ELT
- * @jest-environment jsdom
- */
-describe("Dynamic ListElt", () => {
-  const wrapper = mount(ListElt, {
-    props: {
-      items: [
-        { id: 1, name: "Item 1" },
-        { id: 2, name: "Item 2" },
-        { id: 3, name: "Item 3" }
-      ],
-      dynamic: true
-    }
-  })
-
-  test("must have a props 'dynamic' with 'Boolean' as type & 'true' as default value", () => {
+  test("must have a props 'dynamic' with 'Boolean' as type & 'false' as default value", () => {
     expect(typeof wrapper.props("dynamic")).toBe("boolean")
-    expect(wrapper.props("dynamic")).toBe(true)
-  })
-
-  test("must have a props 'dynamic' with 'true' as value", () => {
-    expect(wrapper.props("dynamic")).toBe(true)
+    expect(wrapper.props("dynamic")).toBe(false)
   })
 })
 
 /**
- * DYNAMIC NESTED LIST ELT
+ * ! DYNAMIC NESTED LIST
  * @jest-environment jsdom
  */
 describe("Dynamic Nested ListElt", () => {
@@ -132,15 +171,37 @@ describe("Dynamic Nested ListElt", () => {
         }
       ],
       dynamic: true
+    },
+    slots: {
+      nested: "Test Nested"
     }
   })
 
-  test("must have a props 'dynamic' with 'Boolean' as type & 'true' as default value", () => {
-    expect(typeof wrapper.props("dynamic")).toBe("boolean")
-    expect(wrapper.props("dynamic")).toBe(true)
+  test("must create an ul element", () => {
+    expect(wrapper.find("ul").exists()).toBe(true)
   })
 
-  test("must have a props 'dynamic' with 'true' as value", () => {
+  test("must create a li element for each item", () => {
+    expect(wrapper.findAll("li").length).toBe(4)
+  })
+  test("must have a props 'items' with 'Object' as type", () => {
+    expect(typeof wrapper.props("items")).toBe("object")
+  })
+
+  test("must have a props 'items' with 'Item 1' as name of first item", () => {
+    expect(wrapper.props("items")[0].name).toBe("Item 1")
+  })
+
+  test("must have a props 'items' with 'Item 2' as name of second item", () => {
+    expect(wrapper.props("items")[0].children[0].name).toBe("Item 2")
+  })
+
+  test("must have a props 'items' with 'Item 3' as name of third item", () => {
+    expect(wrapper.props("items")[0].children[1].name).toBe("Item 3")
+  })
+
+  test("must have a props 'dynamic' with 'Boolean' as type & 'true' as value", () => {
+    expect(typeof wrapper.props("dynamic")).toBe("boolean")
     expect(wrapper.props("dynamic")).toBe(true)
   })
 })
