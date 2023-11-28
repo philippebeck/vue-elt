c9bd5984d9e30ea4b73d218cb88a0de52c1c91e4
Philippe Beck <philippe@philippebeck.net>
2023-11-28 23:38:05 +0400

    Remove all useless files

--- dev/serve.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import Dev from "./serve.vue";
-import VueElt from "@/entry.esm";
-
-const app = createApp(Dev);
-app.use(VueElt).mount("#app");
--- src/entry.js
+++
@@ -1,9 +0,0 @@
-import plugin, * as components from '@/entry.esm';
-
-Object.entries(components).forEach(([componentName, component]) => {
-  if (componentName !== 'default') {
-    plugin[componentName] = component;
-  }
-});
-
-export default plugin;
