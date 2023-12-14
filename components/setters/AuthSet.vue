2e5628fdb0fe4beacff28baf4f8ed6f77378e3f4
Philippe Beck <philippe@philippebeck.net>
2023-12-14 14:06:06 +0400

    Merge signin & forgot pass to AuthSet

--- components/setters/AuthSet.vue
+++ components/setters/AuthSet.vue
@@ -1,5 +1,33 @@
 <template>
-  <form>
+  <form v-if="type === 'signin'">
+    <FieldElt type="email"
+      v-model:value="email"
+      :info="val.INFO_EMAIL">
+      <template #legend>{{ val.LEGEND_EMAIL }}</template>
+      <template #label>{{ val.LABEL_EMAIL }}</template>
+    </FieldElt>
+
+    <FieldElt type="password"
+      v-model:value="pass"
+      :info="val.INFO_PASSWORD">
+      <template #legend>{{ val.LEGEND_PASSWORD }}</template>
+      <template #label>{{ val.LABEL_PASSWORD }}</template>
+    </FieldElt>
+
+    <vue-recaptcha :sitekey="val.RECAPTCHA_KEY"
+      @verify="onVerify">
+      <BtnElt type="button"
+        class="btn-green"
+        :content="val.CONTENT_ENTER"
+        :title="val.TITLE_SIGNIN">
+        <template #btn>
+          <i class="fa-solid fa-door-open fa-lg"></i>
+        </template>
+      </BtnElt>
+    </vue-recaptcha>
+  </form>
+
+  <form v-if="type === 'forgot'">
     <FieldElt type="email"
       v-model:value="email"
       :info="val.INFO_EMAIL"
@@ -27,20 +55,21 @@ import { checkRegex, postData, setError } from "servidio"
 
 import BtnElt from "../elements/BtnElt"
 import FieldElt from "../elements/FieldElt"
-import { VueRecaptcha } from "vue-recaptcha"
+import { VueRecaptcha } from "vue-recaptcha";
 
 export default {
-  name: "ForgotPass",
-  components: { 
+  name: "AuthSet",
+  components: {  
     BtnElt,
     FieldElt,
     VueRecaptcha 
   },
-  props: ["val"],
+  props: ["type", "val"],
 
   data() {
     return {
-      email: ""
+      email: "",
+      pass: ""
     }
   },
 
@@ -48,20 +77,26 @@ export default {
     /**
      * ? ON VERIFY
      * Checks the validity of the given response 
-     * and sends new password if successful
+     * and performs signin if successful
      *
      * @param {any} response - The response to verify.
      */
     onVerify(response) {
-      const { CHECK_EMAIL, REGEX_EMAIL, API_URL } = this.val;
+      const { CHECK_EMAIL, REGEX_EMAIL, CHECK_PASS, REGEX_PASS, API_URL } = this.val;
 
-      if (checkRegex(this.email, CHECK_EMAIL, REGEX_EMAIL)) {
+      if (this.type === "signin" &&
+          checkRegex(this.email, CHECK_EMAIL, REGEX_EMAIL) && 
+          checkRegex(this.pass, CHECK_PASS, REGEX_PASS)
+          ||
+          this.type === "forgot" &&
+          checkRegex(this.email, CHECK_EMAIL, REGEX_EMAIL)) {
+
         const URL = `${API_URL}/auth/recaptcha`;
 
         postData(URL, { response })
           .then(result => {
             if (result.success) {
-              this.forgotPass();
+              this.signIn();
 
             } else {
               alert("Failed captcha verification");
@@ -72,6 +107,27 @@ export default {
             this.$router.go();
           });
       }
+    },
+
+    /**
+     * ? SIGN IN
+     * Signs in the user.
+     */
+    signIn() {
+      const URL   = `${this.val.API_URL}/auth`;
+      const data  = new FormData();
+
+      data.append("email", this.email);
+      data.append("pass", this.pass);
+
+      postData(URL, data)
+        .then((res) => {
+          localStorage.setItem("userToken", JSON.stringify(res.token));
+          localStorage.setItem("userId", JSON.stringify(res.userId));
+
+          this.$router.go();
+        })
+        .catch(err => { setError(err) });
     },
 
     /**
