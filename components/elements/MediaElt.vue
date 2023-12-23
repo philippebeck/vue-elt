fe56511d8d59f1c543ccf244c737bc5315f5daa9
Philippe Beck <philippe@philippebeck.net>
2023-12-24 02:40:47 +0400

    Fix design details to media element figcaption

--- components/elements/MediaElt.vue
+++ components/elements/MediaElt.vue
@@ -133,9 +133,9 @@ blockquote {
 figcaption {
   --ve-media-figcaption-display: flex;
   --ve-media-figcaption-flex-flow: column nowrap;
-  --ve-media-figcaption-place-content: center center;
+  --ve-media-figcaption-place-content: center;
   --ve-media-figcaption-max-width: 1000px;
-  --ve-media-figcaption-font-size: 2.5rem;
+  --ve-media-figcaption-font-size: 2rem;
   --ve-media-figcaption-font-style: normal;
   --ve-media-figcaption-color: var(--ani-slate);
 }
